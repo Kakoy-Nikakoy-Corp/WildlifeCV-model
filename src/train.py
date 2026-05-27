c8c82bd5b9f2acd7ec3d95397d03d4b70f731fba
Maksim Shchibrik <partycle2006nt@gmail.com>
2026-05-28 02:29:40 +0500

    prepare for 4090 training

--- src/train.py
+++ src/train.py
@@ -3,7 +3,11 @@ from ultralytics import settings
 from pathlib import Path
 import mlflow
 
-from config import EXPERIMENT_NAME, ARTIFACT_URI, MODEL_PATH, DATASET_URL
+from config import EXPERIMENT_NAME, ARTIFACT_URI, MODEL_PATH, DATASET_URL, RUN
+
+settings.update({
+    'datasets_dir': str(Path('./dataset').resolve())
+})
 
 if __name__ == '__main__':
     # Create only if it doesn't exist
@@ -15,9 +19,40 @@ if __name__ == '__main__':
         )
 
     mlflow.set_experiment(EXPERIMENT_NAME)
-    print(mlflow.get_artifact_uri())
-
-    settings.update({'datasets_dir': str(Path('./dataset').resolve())})
 
     model = YOLO(MODEL_PATH)
-    model.train(cfg="data/params.yaml", name=EXPERIMENT_NAME, data=DATASET_URL)
+    model.train(cfg="data/params.yaml", project=EXPERIMENT_NAME, data=DATASET_URL, name=RUN)
+
+    mlflow.end_run()
+    with mlflow.start_run(run_name=RUN):
+        results = model.val(
+            cfg="data/params.yaml",
+            project=EXPERIMENT_NAME,
+            name=RUN,
+            data=DATASET_URL,
+            split="test"
+        )
+
+        # Base metrics
+        precision = results.box.mp
+        recall = results.box.mr
+
+        # F1 score
+        f1 = (
+            2 * precision * recall / (precision + recall)
+            if (precision + recall) > 0
+            else 0
+        )
+
+        metrics = {
+            "mAP50": results.box.map50,
+            "mAP50-95": results.box.map,
+            "Precision": precision,
+            "Recall": recall,
+            "F1": f1,
+            "Preprocess_ms": results.speed["preprocess"],
+            "Inference_ms": results.speed["inference"],
+            "Postprocess_ms": results.speed["postprocess"],
+        }
+
+        mlflow.log_metrics(metrics)
